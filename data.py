de4a5a038a298fd67527d41cb8d899d679d2f1f7
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2021-04-01 23:57:41 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
-#Insert Zoom links below and mention exact joining and leaving time
+# Insert Zoom links below and mention exact joining and leaving time
+
 lst = [
     ["link1", "2:00", "2:30"],
     ["link2", "2:31", "3:00"],
     ["link3", "3:01", "3:30"]
 ]
+
